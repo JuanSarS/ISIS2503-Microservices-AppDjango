2cb8c1d90e0e5e008e8ee1054920ba8e751d0fbb
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2024-05-18 20:09:02 -0500

    Update settings.py

--- places/monitoring/settings.py
+++ places/monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get("PLACES_DB", "places_db"),
-        'USER': os.environ.get("PLACES_DB_USER", "places_user"),
-        'PASSWORD': os.environ.get("PLACES_DB_PASSWD", "isis2503"),
-        'HOST': os.environ.get("PLACES_DB_HOST", "10.128.0.80"),
-        'PORT': os.environ.get("PLACES_DB_PORT", "5432")
+        'NAME':  "places_db",
+        'USER': "places_user",
+        'PASSWORD':  "isis2503",
+        'HOST':  "10.128.0.80",
+        'PORT': "5432"
     }
 }
 
